57b3c201958776fea0e6b95d59e083dcddea72c8
cecilia <cast.cv@gmail.com>
2023-11-21 20:02:06 -0300

    listo para render

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const app = express();
 const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
-app.listen(3030, () => {
-  console.log("Servidor corriendo en puerto 3030!");
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("servidor corriendo en el puerto ${port}"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./views/home.html"));
