e5a2ceaddd68750554a2f03eaea7eb2e25d3a1a4
cecilia <cast.cv@gmail.com>
2023-11-21 19:44:26 -0300

    proyecto mercado liebre completo

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.listen(3030, () => {
+  console.log("Servidor corriendo en puerto 3030!");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./views/home.html"));
+});
+
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/registro.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
