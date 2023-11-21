c4241c42b265e1a4b4f3ae26b4608fedea92b5d8
cecilia <cast.cv@gmail.com>
2023-11-21 20:51:11 -0300

    correccion package

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+const { prototype } = require("events");
 const express = require("express");
 const path = require("path");
 const app = express();
 const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("servidor corriendo en el puerto ${port}"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(` servidor funcionando en el puerto ${PORT} `)
+);
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./views/home.html"));
